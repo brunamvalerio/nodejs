f24d26f543e7b03184c833a13b4ca3642c5fb673
Bruna <brunamvalerio01@gmail.com>
2025-01-31 07:30:51 -0300

    Aula 72. N11 Listando os usuários no banco do banco NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,53 @@
-
 let NeDB = require('nedb');
 
-// Isso faz com que o banco de dados seja carregado automaticamente ao iniciar o servidor
+
 let db = new NeDB({
-    filename: 'users.db',  // Nome do arquivo onde os dados serão salvos
-    autoload: true          
+    filename: 'users.db',
+    autoload: true  // Autoload garante que o banco de dados seja carregado ao inicializar o servidor
 });
 
 module.exports = (app) => {
 
-    
-    
+    // Rota GET para listar todos os usuários
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;  
-        res.setHeader('Content-Type', 'application/json');  
+        // Realiza a consulta no banco de dados através do nome name 
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
 
-        
-        res.json({
-            users:[{
-                name: 'Hcode',  
-                email: 'contato@hcode.com.br',  
-                id: 1  
-            }]
-        });
-
-    });
-
-    
-    
-    app.post('/users', (req, res) => {
-
-        // Inserindo os dados recebidos na requisição ('req.body') no banco de dados
-        db.insert(req.body, (err, user) => {
-
-            // Se ocorrer um erro durante a inserção dos dados, ele será mostrado no console
+            // Se houver erro na consulta
             if (err) {
-                console.log(`erro: ${err}`);
+                console.log(`error: ${err}`);
                 res.status(400).json({
                     error: err 
                 });
             } else {
-                
-                
-                res.status(200).json(user);  
+                // Se não houver erro, retorna a lista de usuários encontrados
+                res.statusCode = 200;  // Código de sucesso HTTP
+                res.setHeader('Content-Type', 'application/json');  // Define o tipo de resposta como JSON
+                res.json({
+                    users  // Resposta com a lista de usuários no formato JSON
+                });
+            }
 
+        });
+
+    });
+
+    // Rota POST para criar um novo usuário
+    app.post('/users', (req, res) => {
+
+        // Inserção dos dados recebidos na requisição ('req.body') no banco de dados
+        db.insert(req.body, (err, user) => {
+
+            // Se ocorrer um erro durante a inserção
+            if (err) {
+                console.log(`erro: ${err}`);
+                res.status(400).json({
+                    error: err  // Retorna o erro para o cliente
+                });
+            } else {
+                // Se a inserção for bem-sucedida, retorna o usuário inserido
+                res.status(200).json(user);  // Retorna os dados do usuário criado como resposta
             }
 
         });
@@ -52,6 +55,4 @@ module.exports = (app) => {
     });
 
 };
-
-
 
